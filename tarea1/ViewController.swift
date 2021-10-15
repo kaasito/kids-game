fceaa2a416fb01bc5c29b0279dc46bcbd87bbfbd
Lucas <lucasromeromagana@gmail.com>
2021-10-15 14:21:00 +0200

    Initial Commit

---
+++ tarea1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tarea1
+//
+//  Created by Lucas Romero Magaña on 15/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
